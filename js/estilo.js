0ee00bda078634e3678e49a2d949968e9b8c19a4
Nazareno Soto <nazitasoto99@gmail.com>
2022-08-29 22:38:17 -0300

    Agregué una funcion

--- js/estilo.js
+++ js/estilo.js
@@ -1,3 +1,5 @@
+//Desafio
+
 alert(`¿No sabés multiplicar?`)
 alert(`Tranqui, yo te ayudo`)
 let numeroMultiplicar = parseInt(prompt('Ingresá un numero que quieras multiplicar'))
@@ -24,3 +26,41 @@ do{
     }
     
 }while((condicion) && (contador < 10))
+
+
+/*Funcion*/
+function preguntaSecret(){
+    let pregunta = prompt("¿Querés contarme un secreto? rta si o no")
+    switch (pregunta){
+        case "si": 
+        let respuesta = "Perfecto, dime tu secreto"
+        alert(respuesta)
+        secretito()
+        break;
+        case "no": 
+        let respuestaNo = "Está bien :("
+        alert(respuestaNo);
+        break;
+        default:
+        alert("Por favor responder con si o no")
+        preguntaSecret()
+        break;
+    }}
+    preguntaSecret()
+    
+    function secretito(){
+    let secretoIntro = prompt("Escribe tu secreto")
+    
+    if (secretoIntro >=639){
+        alert("Solo esto tenés para contar?");
+    }
+    
+    else(
+        alert ("Bien..."))
+        let secret = prompt("Querés contarme otro? rta si o no")
+        if (secret == "si") {
+            secretito()
+        }else(
+            alert("Bueno mi rey, nos vemos, ¡cuidate!"))
+    }
+    
