441e6f737aa6dc3f8e24068f2aef02d704097274
Nazareno Soto <nazitasoto99@gmail.com>
2022-10-13 20:43:38 -0300

    Empecé a hacer mi portfolio

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+//Loader (Esto lo investigué en un lugar externo al curso)
+window.addEventListener('load', () => {
+    const contenedor_loader = document.querySelector ('.contenedor_loader')
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = 'hidden'
+})
